a4c98db079e1261b594766d48fff4a26e189b8f5
Janluc <Janlucsaneaux@gmail.com>
2021-06-09 20:24:58 -0400

    Add nav bar functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import createHomeContent from './home';
 import './style.css';
 
 const content = document.querySelector('#content');
+let currentContent = createHomeContent();
+
+let selectedTab;
 
 const makeNav = () => {
   const header = document.createElement('header');
@@ -11,14 +14,18 @@ const makeNav = () => {
   const navMenu = document.createElement('li');
   const navContact = document.createElement('li');
 
+  selectedTab = navHome;
+
   navList.classList.add('nav-list')
 
   navHome.textContent = 'Home';
+  navHome.addEventListener('click', () => updateCurrentPage('home', navHome));
   
   navMenu.textContent = 'Menu';
+  navMenu.addEventListener('click', () => updateCurrentPage('menu', navMenu));
   
   navContact.textContent = 'Contact';
-
+  navContact.addEventListener('click', () => updateCurrentPage('contact', navContact));
 
   navList.append(navHome, navMenu, navContact);
   nav.append(navList);
@@ -27,6 +34,31 @@ const makeNav = () => {
   return header
 }
 
+const removeContent = () => {
+  content.removeChild(currentContent);
+}
+
+const updateCurrentPage = (newPage, newTab) => {
+  
+  let updatedPage;
+  if (selectedTab != newTab) {
+    if(newPage === 'menu') updatedPage = makeMenuPage()
+    else if(newPage === 'contact' ) updatedPage = makeContactPage();
+    else if(newPage === 'home') updatedPage = createHomeContent();
+  }
+
+  if(!!updatedPage) {
+    removeContent();
+    selectedTab.classList.remove('selected-nav');
+    selectedTab = newTab;
+    selectedTab.classList.add('selected-nav')
+
+    currentContent = updatedPage
+    
+    content.appendChild(currentContent);
+  }
+}
+
 document.body.prepend(makeNav());
 
-content.append(createHomeContent());
+content.append(currentContent);
