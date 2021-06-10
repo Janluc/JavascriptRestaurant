ab7ea43286be1be492fbb9ff898da689c7e74aa5
Janluc <Janlucsaneaux@gmail.com>
2021-06-09 21:06:58 -0400

    Add styling to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,23 @@
 const makeContactPage = () => {
-  const content = document.createElement('div')
-  content.classList.add('center-page')
+  const container = document.createElement('div');
+  const content = document.createElement('div');
+  container.classList.add('contact-page');
+  content.classList.add('center-page');
+
   const contactTitle = document.createElement('h1');
-  contactTitle.textContent = 'Contact Us'
+  contactTitle.textContent = 'Contact Us';
 
   const contactName = document.createElement('h3');
   const contactEmail = document.createElement('h3');
   const contactNumber = document.createElement('h3');
 
   contactName.textContent = 'Mickey';
-  contactEmail.textContent = 'MickeyBurgers@MickeysBurgerShack.com';
-  contactNumber.textContent = '800-252-2000';
+  contactEmail.textContent = 'E-mail: MickeyBurgers@MickeysBurgerShack.com';
+  contactNumber.textContent = 'Phone: 800-252-2000';
 
   content.append(contactTitle, contactName, contactEmail, contactNumber);
-  return content;
+  container.append(content);
+  return container;
 }
 
 export default makeContactPage;
